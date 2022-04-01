cb22b9e2098d4c8382db47efce58904ce1eddd88
True-Freedom <enigma@xaker.ru>
2022-04-01 21:13:47 +0300

    mod 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using std::cin;
 using std::cout;
@@ -7,7 +8,11 @@ using std::endl;
 int main()
 {
 	cout << "Hello, World!" << endl;
-	cin.get();
+
+	std::string str;
+	cin >> str;
+	cout << "input string: " << str << endl;
+
 	cout << "Bye..." << endl;
 	return 0;
 }
